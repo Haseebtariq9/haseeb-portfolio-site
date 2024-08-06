6db23cc3f37247ab4414fc0f0c272bec1a775e35
Haseebtariq9 <haseebtariq091199@gmail.com>
2024-08-06 16:51:11 +0500

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,93 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2021 - Present",
+    role: "Associate Developer",
+    company: "Unique Track Pvt. Ltd.",
+    description: `Responsible for developing and maintaining websites using the Enfold WordPress theme. Set up a WhatsApp Chatbot using the WhatsApp Web JS library, Node.js, and Express.js. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Enfold", "Node.js", "Express.js", "SQL", "Docker", "HTML", "CSS", "JavaScript"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "CRO/Digital Strategist",
+    company: "Unique Track Pvt. Ltd.",
+    description: `Served as a Customer Relationship Officer and Digital Strategist. Handled customer queries and complaints, providing timely and effective solutions. Developed and implemented digital marketing strategies to drive traffic and increase conversions. Conducted call quality audits to ensure customer service excellence and provided feedback for improvement. Identified areas for enhancement in inbound and outbound calls to generate leads and build trustworthy client relationships.`,
+    technologies: ["Customer Support", "Call Quality Assurance", "Digital Marketing", "SEO", "Adobe Photoshop"],
+  },
+  {
+    year: "2019 - 2020",
+    role: "Call Center Agent",
+    company: "Precision Estimation",
+    description: `Engaged with customers to generate leads for the company. Managed inbound and outbound calls to assist customers with estimation inquiries, providing recommendations based on their needs. Worked collaboratively with team members to meet and exceed sales targets and customer satisfaction goals. Trained new staff members.`,
+    technologies: ["Spoken English", "Training", "Outbound Calls", "Lead Generation"],
+  },
+  {
+    year: "2019",
+    role: "Customer Support Officer",
+    company: "Unique Track Pvt. Ltd.",
+    description: `Provided customer support and assistance via inbound calls. Routed customer complaints and inquiries to the relevant departments. Worked with cross-functional teams to resolve customer issues and improve service quality. Generated leads and provided information on the company's products and services.`,
+    technologies: ["Lead Generation", "Customer Support", "Customer Service"],
+  },
+  {
+    year: "2018 - 2019",
+    role: "Class Teacher/Lecturer",
+    company: "New Kashmir School, Mint Stop, Lahore",
+    description: `Served as the class in-charge for 9th grade, responsible for developing and delivering lesson plans, assessing student performance, and providing feedback to students and parents. Lectured on Computer Science for 9th and 10th grades, teaching from the BISE Lahore Computer Science textbook, including practicals, programming languages, and software development concepts.`,
+    technologies: ["Curriculum Development", "Student Engagement", "Communication", "Classroom Management", "Computer Science"],
+  },
+];
+
+
+export const PROJECTS = [
+  {
+    title: "Dr Aisha Ghias",
+    image: project1,
+    description:
+      "A Website with the Portfolio of Docter Aisha Ghias, Services provided by her, Integerated YouTube Channel with website, blogs and contact page. The website is built using Enfold Theme, Wordpress CMS, Avia Builders, Plugin Integerations.",
+    technologies: ["Youtube Integration", "Wordpress CMS", "Avia Builder", "WP Classic Editor", "Enfold Theme", "Theme Customization"],
+  },
+  {
+    title: "Unique Track",
+    image: project2,
+    description:
+      "An Website of Vehicle Tracking Company with the services provided by the company, contact page, blogs and user login page. The website is built using Enfold Theme, Wordpress CMS, Avia Builder, Plugin Integerations.",
+    technologies: ["Wordpress CMS", "Avia Builder", "WP Classic Editor", "Enfold Theme", "Theme Customization"],
+  },
+  {
+    title: "WhatsApp Chatbot",
+    image: project3,
+    description:
+      "A WhatsApp Chatbot that provides information about the doctor services and products with appointment booking and live chat option. The chatbot is built using the WhatsApp Web JS library, Node.js, and Express.js.",
+    technologies: ["Node.js", "Express.js", "Git Hub", "WhatsApp Web JS Library", "Chatbot Development", "API Integration", "Live Chat", "Appointment Booking", "Customer Support"],
+  },
+  {
+    title: "Royal Gulf Tents & Tarpaulins",
+    image: project4,
+    description:
+      "A website for showcasing their digital presence, with services provided by Royal Gulf Tents",
+    technologies: ["Wordpress CMS", "Avia Builder", "WP Classic Editor", "Enfold Theme", "Theme Customization"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project5,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind CSS", "Framer Motion", "Node.js"],
+  },
+];
+
+
+export const CONTACT = {
+  address: "Lahore, Punjab, Pakistan",
+  phoneNo: "+923261430007 ",
+  email: "muhammadhaseebtariq2@gmail.com",
+};
